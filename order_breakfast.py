4fc6ab33f97664dea97456922d37eb31e115a019
grabania <grabania.work@gmail.com>
2020-03-22 21:28:04 +0000

    Add valid input

--- order_breakfast.py
+++ order_breakfast.py
@@ -6,9 +6,26 @@
 # elif response == "pancakes":
 #     print("Pancakes it is!")
 
-response = input(
-    "Please place your order. Would you like waffles or pancakes?\n")
-if response.lower() == "waffles":
-    print("Waffles it is!")
-elif response.lower() == "pancakes":
-    print("Pancakes it is!")
+# or:
+# while True:
+#     response = input(
+#         "Please place your order. Would you like waffles or pancakes?\n").lower()
+#     if response == "waffles":
+#         print("Waffles it is!")
+#         break
+#     elif response == "pancakes":
+#         print("Pancakes it is!")
+#         break
+#     else:
+#         print("Sorry, I don't understand.")
+
+response = ""
+while response != "waffles" and response != "pancakes":
+    response = input(
+        "Please place your order. Would you like waffles or pancakes?\n")
+    if response.lower() == "waffles":
+        print("Waffles it is!")
+    elif response.lower() == "pancakes":
+        print("Pancakes it is!")
+    else:
+        print("Sorry, I don't understand.")
